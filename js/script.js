5e896eb816cf23c75bb9c11244052977720ecbf8
Jairo Filho <jairofilho79@gmail.com>
2023-03-03 10:36:18 -0300

    updating files structure

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * 1: Trabalho
+ * 2: Intervalo
+ */
+let abaAtual = 1 
+
+/**
+ * 1: Iniciar
+ * 2: Pausar
+ * 3: Continuar
+*/
+let estadoAtual = 1
+
+const updateTime = 1000
+
+let interval
+let time = timeMapper[abaAtual]
