3385b88ceec0b91adf0e70be855893826c904bbd
eusamdeoliveira <samaraicm1997@gmail.com>
2023-03-03 15:22:03 -0300

    dark mode workando belezinha

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ const updateTime = 1000
 
 let interval
 let time = timeMapper[abaAtual]
+
+// Dark mode
+let modeAtual = 1
+
+function dark(){
+    modeAtual = 2
+    updateMode()
+}
+
+function light(){
+    modeAtual = 1
+    updateMode()
+}
+
+function updateMode() {
+    document.getElementsByClassName('darkmode')[0].innerHTML = modeMapper[modeAtual]
+    console.log(modeAtual)
+  }
+
+const modeMapper = {
+    1: `<button id="dark" onclick="dark()" >🌙</button>
+    <link rel="stylesheet" href="style.css"></link>`,
+    2: `<button id="light" onclick="light()">☀️</button>
+    <link rel="stylesheet" href="dark.css"></link>`
+}
